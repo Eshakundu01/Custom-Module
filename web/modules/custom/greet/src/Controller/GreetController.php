3d6edd9c4cdcc4f8f5b62fcdb9d9996cb77fbaab
Esha Kundu <esha.kundu@innoraft.com>
2023-06-26 18:30:58 +0530

    FT2023-339: Add comments and changes

--- web/modules/custom/greet/src/Controller/GreetController.php
+++ web/modules/custom/greet/src/Controller/GreetController.php
@@ -7,20 +7,50 @@ namespace Drupal\greet\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Returns responses for Greet routes.
+ */
 class GreetController extends ControllerBase {
-  public function overview() {
-    return array(
+
+  /**
+   * Displays the markup.
+   *
+   * @return array
+   *   The markup content is returned.
+   */
+  public function view() {
+    return [
       '#type' => 'markup',
-      '#markup' => 'Hello '. \Drupal::currentUser()->getDisplayName(),
-    );
+      '#markup' => 'Hello World',
+    ];
   }
 
+  /**
+   * Displays the markup with the current users name.
+   *
+   * @return array
+   *   The markup content is returned.
+   */
+  public function overview() {
+    return [
+      '#type' => 'markup',
+      '#markup' => 'Hello '. \Drupal::currentUser()->getDisplayName(),
+    ];
+  }
+
+  /**
+   * Adds new markup content that is going to be displayed with the current
+   * users email address.
+   * 
+   * @return mixed
+   *   The result as per the access is returned.
+   */
   public function add() {
-    if (\Drupal::currentUser()->hasPermission('greet permission 0')) {
-      return array(
+    if (\Drupal::currentUser()->hasPermission('add greet permission')) {
+      return [
         '#type' => 'markup',
         '#markup' => 'Welcome ' . \Drupal::currentUser()->getEmail(),
-      );
+      ];
     }
   }
 }
--- web/modules/custom/greet/src/GreetPermissions.php
+++ web/modules/custom/greet/src/GreetPermissions.php
@@ -4,21 +4,28 @@ namespace Drupal\greet;
 
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 
+/**
+ * Provides dynamic permissions for greet module.
+ */
 class GreetPermissions {
 
   use StringTranslationTrait;
   
   /**
+   * In this function an array is taken which is then looped through to add
+   * permissions dynamically and in case the array index value is 1 restrict
+   * access becomes true.
+   * 
    * @return array
+   *   The greet permissions.
    */
-  public function permissions()
-  {
+  public function permissions() {
     $permissions = [];
 
     $value = ['Add', 'Edit'];
     foreach ($value as $key => $words) {
       $permissions += [
-        "greet permission $key" => [
+        strtolower($words) . " greet permission" => [
           'title' => $this->t('@name permission', ['@name' => $words]),
           'description' => $this->t('This is a sample permission generated dynamically.'),
           'restrict access' => $key == 1 ? true : false,
