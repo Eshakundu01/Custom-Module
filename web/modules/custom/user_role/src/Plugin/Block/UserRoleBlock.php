a656cd1b44f21012f0190c7ff718cb4e399a9af4
Esha Kundu <esha.kundu@innoraft.com>
2023-07-05 19:01:20 +0530

    FT2023-343: Add block task files

---
+++ web/modules/custom/user_role/src/Plugin/Block/UserRoleBlock.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * This creates a custom block.
+ */
+
+namespace Drupal\user_role\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'User Role' block.
+ *
+ * @Block(
+ *   id = "user_role_block",
+ *   admin_label = @Translation("User Role Block"),
+ * )
+ */
+class UserRoleBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $user;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+    $instance->user = $container->get('current_user');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $role = join(",", $this->user->getRoles(TRUE));
+    return [
+      '#markup' => $this->t('Welcome @roles', ['@roles' => $role]),
+    ];
+  }
+}
